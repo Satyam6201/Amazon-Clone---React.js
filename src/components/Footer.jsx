aa058328bba21936f5bfb1f36215d632e77ae611
satyam kumar mishra <satyamkmishraa@gmail.com>
2025-04-16 03:21:35 +0530

    Add Feature

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,25 +14,30 @@ const Footer = () => {
       <div className="footer-content">
         <div className="footer-section">
           <h3>Get to Know Us</h3>
+
           <ul>
             <li><a href="#">About Us</a></li>
             <li><a href="#">Careers</a></li>
             <li><a href="#">Press Releases</a></li>
             <li><a href="#">Amazon Science</a></li>
           </ul>
+
         </div>
 
         <div className="footer-section">
           <h3>Connect with Us</h3>
+
           <ul>
             <li><a href="https://www.facebook.com/profile.php?id=100024550755973">Facebook</a></li>
             <li><a href="https://x.com/satyamkmishraa?t=kATgYsKWGY4_ZJfpr1l7pg&s=09">Twitter</a></li>
             <li><a href="https://www.instagram.com/satyammishra_467">Instagram</a></li>
           </ul>
+
         </div>
 
         <div className="footer-section">
           <h3>Make Money with Us</h3>
+
           <ul>
             <li><a href="#">Sell on Amazon</a></li>
             <li><a href="#">Sell under Amazon Accelerator</a></li>
@@ -42,10 +47,12 @@ const Footer = () => {
             <li><a href="#">Fulfillment by Amazon</a></li>
             <li><a href="#">Advertise Your Products</a></li>
           </ul>
+
         </div>
 
         <div className="footer-section">
           <h3>Let Us Help You</h3>
+
           <ul>
             <li><a href="#">Your Account</a></li>
             <li><a href="#">Returns Centre</a></li>
@@ -53,6 +60,7 @@ const Footer = () => {
             <li><a href="#">Amazon App Download</a></li>
             <li><a href="#">Help</a></li>
           </ul>
+          
         </div>
       </div>
 
